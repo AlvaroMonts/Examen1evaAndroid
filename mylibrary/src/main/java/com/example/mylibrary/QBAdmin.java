98f01dea3fc575b42268769cc9c1e5549f98a4cf
unknown <alvaro.montes@WKSALMPBB720.u-tad.es>
2016-11-24 12:17:51 +0100

    empezando creditos

--- mylibrary/src/main/java/com/example/mylibrary/QBAdmin.java
+++ mylibrary/src/main/java/com/example/mylibrary/QBAdmin.java
@@ -1,7 +1,15 @@
 package com.example.mylibrary;
 
-/**
- * Created by alvaro.montes on 22/11/2016.
- */
-public class QBAdmin {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QBAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qbadmin);
+    }
+
+
 }
